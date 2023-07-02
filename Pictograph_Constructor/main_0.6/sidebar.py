4c6e447dfb1131894a53ace0a71c3b3665c1ae5f
Austen <Austencloud@gmail.com>
2023-07-02 18:31:13 -0500

    debugging pictograph constructor

--- Pictograph_Constructor/main_0.6/sidebar.py
+++ Pictograph_Constructor/main_0.6/sidebar.py
@@ -23,6 +23,39 @@ class Objects_From_Sidebar(QGraphicsSvgItem):
         elif event.button() == Qt.LeftButton:
             self.Drop_Frame_Objects_start_position = event.pos()
 
+            # Create a QDrag object
+            self.drag = QDrag(self)
+            mime_data = QMimeData()
+
+            # Set the file path of the SVG file in the MIME data
+            mime_data.setText(self.svg_file)
+            self.drag.setMimeData(mime_data)
+
+            # Create a QImage with the same dimensions as the item
+            image = QImage(self.boundingRect().size().toSize() * 8, QImage.Format_ARGB32)
+
+            # Create a QPainter and draw the item into the QImage
+            painter = QPainter(image)
+
+            # Render the item using the QPainter
+            renderer = QSvgRenderer(self.svg_file)
+            if not renderer.isValid():
+                print(f"Failed to load SVG file: {self.svg_file}")
+                return
+            renderer.render(painter)
+
+            # End the QPainter
+            painter.end()
+
+            # Convert the QImage to a QPixmap
+            pixmap = QPixmap.fromImage(image)
+
+            # Set the pixmap for the drag
+            self.drag.setPixmap(pixmap)
+
+            # Set the hotspot to the center of the pixmap
+            self.drag.setHotSpot(pixmap.rect().center())
+
     def mouseMoveEvent(self, event):
         if self.in_drop_frame:
             super().mouseMoveEvent(event)
@@ -31,43 +64,8 @@ class Objects_From_Sidebar(QGraphicsSvgItem):
         elif (event.pos() - self.Drop_Frame_Objects_start_position).manhattanLength() < QApplication.startDragDistance():
             return
         else:
-            from drop_frame import Drop_Frame_Objects
-            Drop_Frame_Objects = Drop_Frame_Objects(self)
-            mime_data = QMimeData()
+            self.drag.exec_(Qt.CopyAction | Qt.MoveAction)
 
-            # Set the file path of the SVG file in the MIME data
-            mime_data.setText(self.svg_file)
-
-            # Create a QDrag object
-            drag = QDrag(self)
-            drag.setMimeData(mime_data)
-            drag.setHotSpot((event.pos() - self.dragOffset).toPoint())
-
-            # Create a QImage with the same dimensions as the item
-            image = QImage(self.boundingRect().size().toSize() * 8, QImage.Format_ARGB32)
-
-            # Create a QPainter and draw the item into the QImage
-            painter = QPainter(image)
-
-            # Scale the QPainter
-            painter.scale(8, 8)
-
-            # Render the item using the scaled QPainter
-            self.renderer().render(painter)
-
-            # End the QPainter
-            painter.end()
-
-            # Convert the QImage to a QPixmap
-            pixmap = QPixmap.fromImage(image)
-
-            # Create a new QGraphicsPixmapItem
-            pixmap_item = QGraphicsPixmapItem(pixmap)
-
-            # Set the pixmap for the drag
-            drag.setPixmap(pixmap_item.pixmap())
-
-            drag.exec_(Qt.CopyAction | Qt.MoveAction)
 
     def mouseReleaseEvent(self, event):
         self.setCursor(Qt.OpenHandCursor)
