06cad27090866da7c8a2d98835320ef249e49c96
Austen <Austencloud@gmail.com>
2023-07-06 13:00:08 -0500

    updating pictograph_constructor

--- Pictograph_Constructor/main 0.4/main.py
+++ Pictograph_Constructor/main 0.4/main.py
@@ -39,12 +39,7 @@ class Main_Window(QWidget):
         self.setLayout(hbox)
         self.setWindowTitle('Drag & Drop')
         self.show()
-        self.InfoTracker = QLabel(self)
-        self.InfoTracker.setTextInteractionFlags(Qt.TextSelectableByMouse)
         self.initButtons(vbox)
-        vbox.addWidget(self.InfoTracker)
-        self.infoTrackerInstance = Info_Tracker(self.view, self.InfoTracker)  # pass the view and InfoTracker here
-        self.artboard.changed.connect(lambda: self.infoTrackerInstance.updateInfoTracker()) 
 
 
     def initArrowBox(self):
@@ -161,14 +156,7 @@ class Main_Window(QWidget):
         self.uploadSVGButton = QPushButton("Upload SVG")
         self.uploadSVGButton.clicked.connect(handlers.upload_svg)
         masterbtnlayout.addWidget(self.uploadSVGButton)
-        
 
-        # add text label for infotracker
-        self.infoTracker = QLabel("Info Tracker")
-        self.infoTracker.setFont(QFont('Helvetica', 14))
-        #connect to handler so it updates whenever the state of the artboard changes
-        self.artboard.changed.connect(Info_Tracker.updateInfoTracker)
-        masterbtnlayout.addWidget(self.infoTracker)
 
         button_font = QFont('Helvetica', 14)
 
@@ -182,42 +170,6 @@ class Main_Window(QWidget):
         self.exportAsSVGButton.setFont(button_font)
         self.uploadSVGButton.setFont(button_font)
 
-class Info_Tracker():
-    def __init__(self, view, InfoTracker):
-        self.view = view
-        self.InfoTracker = InfoTracker
-
-    def updateInfoTracker(self):
-        selectedItems = self.view.scene().selectedItems()
-        text = ""
-        for item in selectedItems:
-            if isinstance(item, Arrow_Logic):
-                try:
-                    attributes = item.get_attributes()
-                    text += f"Color: {attributes.get('color', 'N/A')}\n"
-                    text += f"Quadrant: {attributes.get('quadrant', 'N/A').upper()}\n"
-                    text += f"Rotation: {attributes.get('rotation', 'N/A')}\n"
-                    text += f"Type: {attributes.get('type', 'N/A').capitalize()}\n"
-                    text += "\n"
-                except Exception as e:
-                    text += f"Error getting attributes for item: {e}\n"
-            else:
-                text += f"Item is not an instance of Arrow_Logic\n"
-            self.InfoTracker.setText(text)
-            
-            for i in range(len(selectedItems)):
-                for j in range(i+1, len(selectedItems)):
-                    item1 = selectedItems[i]
-                    item2 = selectedItems[j]
-                    if isinstance(item1, Arrow_Logic) and isinstance(item2, Arrow_Logic):
-                        # Check the properties of item1 and item2 to determine the letter
-                        if item1.rotation == item2.rotation:
-                            if item1.type == item2.type == "iso":
-                                text += "Letter: A\n"
-                            elif item1.type == item2.type == "anti":
-                                text += "Letter: B\n"
-                        elif item1.type != item2.type:
-                            text += "Letter: C\n"
 
 
 app = QApplication(sys.argv)
