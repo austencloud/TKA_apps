99857b3c46c5c3ecddeedc317c1fa5dca23e3fce
Austen <Austencloud@gmail.com>
2023-07-06 12:45:32 -0500

    updating pictograph_constructor

--- Pictograph_Constructor/main 0.4/main.py
+++ Pictograph_Constructor/main 0.4/main.py
@@ -8,7 +8,6 @@ from artboard_events import Artboard_Events
 from button_handlers import Button_Handlers
 from grid import Grid
 from PyQt5.QtWidgets import QFileDialog
-from info_tracker import Info_Tracker
 
 class Main_Window(QWidget):
     ARROW_DIR = 'images\\arrows'
@@ -182,6 +181,41 @@ class Main_Window(QWidget):
         self.exportAsPNGButton.setFont(button_font)
         self.exportAsSVGButton.setFont(button_font)
         self.uploadSVGButton.setFont(button_font)
+
+class Info_Tracker():
+    def __init__(self, view, InfoTracker):
+        self.view = view
+        self.InfoTracker = InfoTracker
+
+    def updateInfoTracker(self):
+        print(type(self.view))
+        print(type(self.view.scene()))
+        selectedItems = self.view.scene().selectedItems()
+        text = ""
+        for item in selectedItems:
+            if isinstance(item, Arrow_Logic):
+                attributes = item.get_attributes()
+                text += f"Color: {attributes['color']}\n"
+                text += f"Quadrant: {attributes['quadrant'].upper()}\n"
+                text += f"Rotation: {attributes['rotation']}\n"
+                text += f"Type: {attributes['type'].capitalize()}\n"
+                text += "\n"
+            self.InfoTracker.setText(text)
+            
+            for i in range(len(selectedItems)):
+                for j in range(i+1, len(selectedItems)):
+                    item1 = selectedItems[i]
+                    item2 = selectedItems[j]
+                    if isinstance(item1, Arrow_Logic) and isinstance(item2, Arrow_Logic):
+                        # Check the properties of item1 and item2 to determine the letter
+                        if item1.rotation == item2.rotation:
+                            if item1.type == item2.type == "iso":
+                                text += "Letter: A\n"
+                            elif item1.type == item2.type == "anti":
+                                text += "Letter: B\n"
+                        elif item1.type != item2.type:
+                            text += "Letter: C\n"
+            
 
 
 app = QApplication(sys.argv)
